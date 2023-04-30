68affb2447d0a0d32979e2744a3119e05ac9dcbb
angelinadegay <59676843+angelinadegay@users.noreply.github.com>
2023-04-30 16:24:42 -0400

    Update BSTIterator.java

--- ProgProjFive/src/iterators/BSTIterator.java
+++ ProgProjFive/src/iterators/BSTIterator.java
@@ -4,27 +4,23 @@ import java.util.Iterator;
 import interfaces.BSTInterface.TraversalType;
 import adts.BinarySearchTree;
 
-public class BSTIterator<E extends Comparable<E>> implements Iterator<E> 
-{
+public class BSTIterator<E extends Comparable<E>> implements Iterator<E> {
     protected BinarySearchTree<E> bst;
     protected TraversalType traversalType;
 
-    public BSTIterator(BinarySearchTree<E> bst, TraversalType traversalType) 
-    {
+    public BSTIterator(BinarySearchTree<E> bst, TraversalType traversalType) {
         this.bst = bst;
         this.traversalType = traversalType;
         bst.setTraversalType(traversalType);
     }
 
     @Override
-    public boolean hasNext()
-    {
-        return !bst.isEmpty();
+    public boolean hasNext() {
+        return bst.peekNext(traversalType) != null;
     }
 
     @Override
-    public E next()
-    {
+    public E next() {
         return bst.getNext(traversalType);
     }
 }
